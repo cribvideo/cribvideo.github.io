a8631687c4138b0c8778152275cbb9e138a9e979
Ejaz <ejazali4@protonmail.ch>
2021-09-27 00:10:18 +0100

    ✨

--- android/index.js
+++ android/index.js
@@ -14,15 +14,36 @@ function initialiseHome(from = "none") {
         homeElem.className = "home slideUpIn"
     }
     homeElem.scrollTo(0, 100);
-
+    
     homeElem.addEventListener('scroll', homeElemScrollListener);
+    midElem.addEventListener('scroll', midElemScrollListener);
+    console.log("evt listener")
 
+    let cameraInitialised = 0;
+
+    function midElemScrollListener() {
+        if (midElem.scrollTop == 0) {
+            if (cameraInitialised == 0) {
+                cameraInitialised = 1;
+                initialiseCamera(true, () => {
+                    console.log("Camera Initialised");
+                });
+            }
+        }
+    }
 
     function homeElemScrollListener() {
         if (homeElem.scrollTop == 0) {
             homeElem.className = "home slideDown";
-            initialiseCamera();
+            if (cameraInitialised == 0) {
+                initialiseCamera(true, () => {
+                    initialiseCamera();
+                });
+            } else {
+                initialiseCamera();
+            }
             homeElem.removeEventListener('scroll', homeElemScrollListener);
+            midElem.removeEventListener('scroll', midElemScrollListener);
         }
     }
 }
@@ -31,17 +52,27 @@ function initialiseChat() {
     const chatElem = document.getElementById("chat");
     chatElem.className = "chat slideRightIn";
 
+    const chatBackButton = document.getElementById("chat-back-button");
     chatElem.addEventListener('scroll', chatElemScrollListener)
+
+    chatBackButton.addEventListener('click', chatBackElemScrollListener);
+
+    function chatBackElemScrollListener() {
+        chatElem.className = "chat slideRightOut";
+        chatElem.removeEventListener('scroll', chatElemScrollListener);
+        chatBackButton.removeEventListener('click', chatElemScrollListener);
+    }
 
     function chatElemScrollListener() {
         if (chatElem.scrollLeft == 0) {
             chatElem.className = "chat slideRightOut";
             chatElem.removeEventListener('scroll', chatElemScrollListener);
+            chatBackButton.removeEventListener('click', chatElemScrollListener);
         }
     }
 }
 
-function initialiseCamera() {
+function initialiseCamera(prepare = false, prepareCb = 'none') {
     var video = document.getElementById("camera-video");
     video.setAttribute('playsinline', '');
     video.setAttribute('autoplay', '');
@@ -52,21 +83,37 @@ function initialiseCamera() {
     var constraints = {
         audio: false,
         video: {
-            facingMode: facingMode
+            optional: [
+                {minWidth: 320},
+                {minWidth: 640},
+                {minWidth: 1024},
+                {minWidth: 1280},
+                {minWidth: 1920},
+                {minWidth: 2560},
+            ],
         }
     };
 
-    navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
-        video.srcObject = stream;
-        document.getElementById("camera").className = "camera screen";
-    });
-
     const cameraElem = document.getElementById("camera");
+    
+    if (prepare == true) {
+        navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+            video.srcObject = stream;
+            if (prepareCb != 'none') {
+                prepareCb();
+            }
+        });
+    } else {
+        document.getElementById("camera").className = "camera screen";
+        cameraElem.addEventListener('scroll', cameraElemScrollListener);
+    }
 
-    cameraElem.addEventListener('scroll', cameraElemScrollListener);
+
 
     function cameraElemScrollListener() {
         if (cameraElem.scrollTop == 0) {
+            cameraElem.removeEventListener('scroll', cameraElemScrollListener);
+            initialiseHome("camera");
             cameraElem.className = "hidden";
             const stream = video.srcObject;
 
@@ -76,8 +123,6 @@ function initialiseCamera() {
                 track.stop();
             });
 
-            initialiseHome("camera");
-            cameraElem.removeEventListener('scroll', cameraElemScrollListener);
         }
     }      
 }
@@ -97,5 +142,4 @@ setInterval(() => {
         lastWidth = window.innerWidth;
     }
 }, 10);
-
 appLoad();
