349bbb8a522cd9a69bc5c54c25a8b8fb99e9b9a9
Ejaz Ali <ejazali4@protonmail.ch>
2021-10-12 22:25:34 +0100

    g

--- android-sw.js
+++ android-sw.js
@@ -1,30 +1,23 @@
 var UI_CACHE_STORE = `CA0.1i`;
-var data_cache_store = `cribdata`
-var urlsToCache = [
-  '/android/',
-  '/android/index.js',
-  '/android/index.css'
-];
+var data_cache_store = `cribdata`;
+var urlsToCache = ["/android/", "/android/index.js", "/android/index.css"];
 
-const dontCache = [
-  "/api/v1/updates"
-]
+const dontCache = ["/api/v1/updates"];
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
   // Perform install steps
   event.waitUntil(
-    caches.open(UI_CACHE_STORE)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(UI_CACHE_STORE).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-      fetch(event.request).then((response) => {
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
         let currentStore = UI_CACHE_STORE;
 
         if (event.request.url.startsWith("https://cribapi.ceccun.com")) {
@@ -35,8 +28,9 @@ self.addEventListener('fetch', function (event) {
           cache.put(event.request, response.clone());
           return response;
         });
-      }).catch(function () {
+      })
+      .catch(function () {
         return caches.match(event.request);
-      }),
-    );
+      })
+  );
 });
